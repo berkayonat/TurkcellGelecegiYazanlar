53174d9223e1a19b8b2a52fb56436dfe4014d241
onat <onat@DESKTOP-B14JIU3>
2023-03-14 18:57:44 +0300

    Proje dosyası ekle.

---
+++ SifreGüvenlikDenetimi/Program.cs
@@ -0,0 +1,46 @@
+﻿
+while (true)
+{
+
+    Console.WriteLine("Bir sifre giriniz");
+    string word = Console.ReadLine();
+    int digits = 0;
+    int letters = 0;
+    int specialCharacters = 0;
+    if (word != null && word.Length >= 6)
+    {
+        foreach (char c in word)
+        {
+            if (char.IsDigit(c))
+            {
+                digits++;
+            }
+            else if (char.IsLetter(c))
+            {
+                letters++;
+            }
+            else
+            {
+                specialCharacters++;
+            }
+        }
+
+        if (digits == word.Length || letters == word.Length || specialCharacters == word.Length)
+        {
+            Console.WriteLine("Zayıf Sifre");
+
+        }
+        else if (specialCharacters == 0)
+        {
+            Console.WriteLine("Orta Sifre");
+        }
+        else
+        {
+            Console.WriteLine("Güçlü Sifre");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Sifre en az 6 karakter olmali");
+    }
+}
