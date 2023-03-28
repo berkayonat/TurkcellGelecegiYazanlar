80b856b233c10c2479135770dcc862d6de903f51
Berkay Onat Demir <berkayonatdemir@gmail.com>
2023-03-28 17:16:38 +0300

    First Commit

---
+++ SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,38 @@
+﻿public class FileReader
+{
+    private string fileContents;
+
+    public string ReadFromFile(string filePath)
+    {
+        Console.WriteLine("Dosyadan okundu.");
+        return fileContents;
+    }
+}
+
+
+public class FileWriter
+{
+    public void WriteToFile(string filePath, string contents)
+    {
+        Console.WriteLine("Dosyaya yazıldı.");
+    }
+}
+
+
+public class FileProcessor
+{
+    private readonly FileReader _fileReader;
+    private readonly FileWriter _fileWriter;
+
+    public FileProcessor(FileReader fileReader, FileWriter fileWriter)
+    {
+        _fileReader = fileReader;
+        _fileWriter = fileWriter;
+    }
+
+    public void CopyFileContents(string sourceFilePath, string destinationFilePath)
+    {
+        string fileContents = _fileReader.ReadFromFile(sourceFilePath);
+        _fileWriter.WriteToFile(destinationFilePath, fileContents);
+    }
+}
