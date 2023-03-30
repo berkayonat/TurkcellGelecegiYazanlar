eb62f789153cef1eae43a450e89ea9504d7d5535
Berkay Onat Demir <berkayonatdemir@gmail.com>
2023-03-30 20:03:53 +0300

    OCP

---
+++ OpenClosedPrinciple/Program.cs
@@ -0,0 +1,49 @@
+﻿
+public interface IOperation
+{
+    double Calculate(double x, double y);
+}
+
+public class AddOperation : IOperation
+{
+    public double Calculate(double x, double y)
+    {
+        return x + y;
+    }
+}
+
+public class MultiplyOperation : IOperation
+{
+    public double Calculate(double x, double y)
+    {
+        return x * y;
+    }
+}
+
+public class Calculator
+{
+    public double Calculate(IOperation operation, double x, double y)
+    {
+        return operation.Calculate(x, y);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+
+        IOperation addOperation = new AddOperation();
+        double result = calculator.Calculate(addOperation, 3, 4);
+        Console.WriteLine("3 + 4 = " + result);
+
+        IOperation multiplyOperation = new MultiplyOperation();
+        result = calculator.Calculate(multiplyOperation, 3, 4);
+        Console.WriteLine("3 * 4 = " + result);
+
+        Console.ReadKey();
+    }
+}
+
+
